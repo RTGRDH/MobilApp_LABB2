702c12a7a9b7a1bfa914a47874c8d1635181e0ef
carl-bernhardhallberg <cbha@kth.se>
2020-11-22 16:54:29 +0100

    - La till game board layout

--- MobilApp_LABB2/GameScene.swift
+++ MobilApp_LABB2/GameScene.swift
@@ -10,6 +10,19 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    /*
+     * The game board positions
+     *
+     * 03           06           09
+     *     02       05       08
+     *         01   04   07
+     * 24  23  22        10  11  12
+     *         19   16   13
+     *     20       17       14
+     * 21           18           15
+     *
+     */
+    
     override func didMove(to view: SKView) {
         
     }
